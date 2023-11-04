981fa23dc6c60f1c9d836daa8c45ac208357838f
Arvatel <namenori74@yandex.ru>
2023-11-04 12:34:17 +0300

    Homework 1

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello, World!"); // Print "Hello, World!" in the console when the script starts
+    }
+}
